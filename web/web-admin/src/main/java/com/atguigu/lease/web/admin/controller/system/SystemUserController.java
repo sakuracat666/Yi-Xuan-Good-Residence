c4bc7d06da39b6b7b54569deec0915f4e4b75418
huhao <2324552486@qq.com>
2024-09-16 16:14:51 +0800

    保存或更新后台用户信息 摘要：集成commons-codec库并应用MD5加密用户密码
    引入commons-codec依赖项，并在保存或更新系统用户信息时，对用户密码进行MD5加密处理。确保密码在传输过程中的安全性。

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/system/SystemUserController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/system/SystemUserController.java
@@ -14,6 +14,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -75,6 +76,10 @@ public class SystemUserController {
     @Operation(summary = "保存或更新后台用户信息")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdate(@RequestBody SystemUser systemUser) {
+        if (systemUser.getPassword() != null){
+            systemUser.setPassword(DigestUtils.md5Hex(systemUser.getPassword()));
+        }
+        systemUserService.saveOrUpdate(systemUser);
         return Result.ok();
     }
 
