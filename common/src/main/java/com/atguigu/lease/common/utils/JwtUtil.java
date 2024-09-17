998d0466da037d710f012c2089e28578f7b2af27
huhao <2324552486@qq.com>
2024-09-17 16:08:02 +0800

    增加JwtUtil注释 处理JWT令牌生成和验证
    JwtUtil工具已进行重构，包括更新导入语句以使用最新的异常处理。新增`createToken`方法用于创建JWT令牌，该方法接收用户ID和用户名作为参数，并返回一个包含用户信息的JWT令牌。另外，实现了`parseToken`方法来解析和验证令牌的有效性，如果令牌过期或无效，将抛出自定义异常。这些更改提高了JwtUtil工具的健壮性和易用性。
    ```

--- common/src/main/java/com/atguigu/lease/common/utils/JwtUtil.java
+++ common/src/main/java/com/atguigu/lease/common/utils/JwtUtil.java
@@ -1,7 +1,8 @@
 package com.atguigu.lease.common.utils;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.atguigu.lease.common.exception.LeaseException;
+import com.atguigu.lease.common.result.ResultCodeEnum;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
@@ -10,15 +11,48 @@ import java.util.Date;
 public class JwtUtil {
     private static SecretKey secretKey = Keys.hmacShaKeyFor("wOhZtawUZJa4DzRQPgEIcobrDo45L2CT".getBytes());
 
-    public static String createToken(Long userId,String username) {
+
+    /**
+     * 创建JWT令牌
+     * 该方法用于根据用户ID和用户名生成一个JWT令牌，用于后续的身份验证和授权操作
+     *
+     * @param userId 用户ID，用于唯一标识系统中的用户
+     * @param username 用户名，用于展示或验证用户身份信息
+     * @return 返回生成的JWT令牌字符串
+     */
+    public static String createToken(Long userId, String username) {
+        // 构建JWT令牌，设置过期时间、主题、用户信息，并使用HS256算法进行签名
         String jwt = Jwts.builder()
-                .setExpiration(new Date(System.currentTimeMillis() + 36000000))
-                .setSubject("LOGIN_USER")
-                .claim("username", username)
-                .claim("userId", userId)
-                .signWith(secretKey, SignatureAlgorithm.HS256)
-                .compact();
-        return jwt;
+                .setExpiration(new Date(System.currentTimeMillis() + 36000000)) // 设置令牌过期时间为当前时间加上10小时（以毫秒为单位）
+                .setSubject("LOGIN_USER") // 设置令牌主题为LOGIN_USER
+                .claim("username", username) // 在令牌中添加用户名信息
+                .claim("userId", userId) // 在令牌中添加用户ID信息
+                .signWith(secretKey, SignatureAlgorithm.HS256) // 使用预定义的密钥和HS256算法对令牌进行签名
+                .compact(); // 将构建好的JWT对象压缩为紧凑的字符串表示形式
+        return jwt; // 返回生成的JWT令牌
+    }
+
+
+    /**
+     * 解析令牌
+     * 本函数尝试解析给定的JWT令牌如果令牌过期或无效，将抛出相应的异常
+     *
+     * @param token 待解析的JWT令牌
+     * @throws LeaseException 当令牌过期（ExpiredJwtException）或无效（JwtException）时抛出
+     */
+    public static void parseToken(String token){
+        try {
+            // 初始化Jwt解析器，并设置签名密钥
+            JwtParser jwtParser = Jwts.parserBuilder().setSigningKey(secretKey).build();
+            // 执行令牌解析
+            jwtParser.parse(token);
+        }catch (ExpiredJwtException e){
+            // 当令牌过期时，抛出LeaseException，指示令牌已过期
+            throw new LeaseException(ResultCodeEnum.TOKEN_EXPIRED);
+        }catch (JwtException e){
+            // 当令牌无效时，抛出LeaseException，指示令牌无效
+            throw new LeaseException(ResultCodeEnum.TOKEN_INVALID);
+        }
     }
 
     public static void main(String[] args) {
