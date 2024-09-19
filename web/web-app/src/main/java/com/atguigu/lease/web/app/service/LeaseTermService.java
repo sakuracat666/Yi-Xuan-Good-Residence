9991251a152ac7c3c74e04c1fd779fdae356dcd8
huhao <2324552486@qq.com>
2024-09-19 16:35:00 +0800

    根据房间id获取可选获取租期列表

--- web/web-app/src/main/java/com/atguigu/lease/web/app/service/LeaseTermService.java
+++ web/web-app/src/main/java/com/atguigu/lease/web/app/service/LeaseTermService.java
@@ -3,10 +3,18 @@ package com.atguigu.lease.web.app.service;
 import com.atguigu.lease.model.entity.LeaseTerm;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
 * @author liubo
 * @description 针对表【lease_term(租期)】的数据库操作Service
 * @createDate 2023-07-26 11:12:39
 */
 public interface LeaseTermService extends IService<LeaseTerm> {
+    /**
+     * 根据房间id查询租期列表
+     * @param id
+     * @return
+     */
+    List<LeaseTerm> listByRoomId(Long id);
 }
