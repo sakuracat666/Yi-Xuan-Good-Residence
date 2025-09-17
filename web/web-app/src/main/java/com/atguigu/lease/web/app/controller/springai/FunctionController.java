c19fa2576b448fd2daa91482eeec7cd745ad8583
huhao <138741199+sakuracat666@users.noreply.github.com>
2025-09-17 11:44:33 +0800

    refactor(web-app): 删除房间收藏相关的 SQL 文件
    - 删除了 room_collect.sql 和 room_collect_test_data.sql 文件
    - 更新了 data_source_mapping.xml 和 sqldialects.xml 文件中的相关配置
    - 优化了 FunctionController 类的代码格式

--- web/web-app/src/main/java/com/atguigu/lease/web/app/controller/springai/FunctionController.java
+++ web/web-app/src/main/java/com/atguigu/lease/web/app/controller/springai/FunctionController.java
@@ -1,6 +1,5 @@
 package com.atguigu.lease.web.app.controller.springai;
 
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.ai.chat.client.ChatClient;
@@ -15,47 +14,46 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/app/ai/test")
 public class FunctionController {
 
-    @Autowired
-    private ChatModel chatModel;
+        @Autowired
+        private ChatModel chatModel;
 
-
-    /**
-     * ai对话助手
-     *
-     * @param userMessage
-     * @return
-     */
-    @Operation(summary = "ai对话助手")
-    @PostMapping(value = "/function", produces = MediaType.TEXT_HTML_VALUE + ";charset=UTF-8")
-    public Flux<String> function01(@RequestParam("userMessage") String userMessage) {
-        Flux<String> content = ChatClient.builder(chatModel)
-                .build().prompt()
-                .system("""
-                        您是公寓推荐助手。
-                        您能够支持公寓信息查询、房间信息查询、设施信息查询、租约信息查询、房间属性查询等操作，其余功能将在后续版本中添加，如果用户问的问题不支持请告知详情。
-                        在提供公寓信息查询服务之前，您必须从用户处获取如下信息：公寓名称、公寓介绍、区域名称、房间号、手机号、房间属性（朝向、面积等）、所在省份等。
-                        请调用自定义函数执行公寓信息查询、房间属性查询、房间信息查询、设施信息查询、租约信息查询等操作。
-                        请讲中文。
-                        """)
-                .user(userMessage)
-                .functions(
-                        "apartmentOperation", // 根据公寓名称、省份名称、城市名称、区域名称、公寓介绍、查询对应的公寓信息
-                        "attrOperation", // 根据房间属性名称查询对应的属性值
-                        "apartmentInfoOperation", // 根据房间属性值查询对应的房间id
-                        "roomStatusOperation", // 根据房间id查询对应的房间状态
-                        "facilityOperation",// 根据房间id查询对应的配套信息
-                        "roomInfoOperation",// 根据房间id查询对应的房间信息
-                        "roomsByApartmentNameOperation", // 根据公寓名称查询所有房间号
-                        "roomByRentRangeOperation", // 根据租金范围查询房间
-                        "roomByPaymentTypeOperation", // 根据付款方式查询房间
-                        "roomByLeaseTermOperation", // 根据租期查询房间
-                        "availableRoomsByApartmentIdOperation", // 根据公寓ID查询可租房源
-                        "leaseTermInfoOperation", // 根据房间ID查询租期信息
-                        "paymentTypeInfoOperation", // 根据房间ID查询付款方式
-                        "apartmentDetailInfoOperation" // 根据公寓ID查询公寓详细信息
-                )
-                .stream()
-                .content();
+        /**
+         * ai对话助手
+         *
+         * @param userMessage
+         * @return
+         */
+        @Operation(summary = "ai对话助手")
+        @PostMapping(value = "/function", produces = MediaType.TEXT_HTML_VALUE + ";charset=UTF-8")
+        public Flux<String> function01(@RequestParam("userMessage") String userMessage) {
+                Flux<String> content = ChatClient.builder(chatModel)
+                                .build().prompt()
+                                .system("""
+                                                您是公寓推荐助手。
+                                                您能够支持公寓信息查询、房间信息查询、设施信息查询、租约信息查询、房间属性查询等操作，其余功能将在后续版本中添加，如果用户问的问题不支持请告知详情。
+                                                在提供公寓信息查询服务之前，您必须从用户处获取如下信息：公寓名称、公寓介绍、区域名称、房间号、手机号、房间属性（朝向、面积等）、所在省份等。
+                                                请调用自定义函数执行公寓信息查询、房间属性查询、房间信息查询、设施信息查询、租约信息查询等操作。
+                                                请讲中文。
+                                                """)
+                                .user(userMessage)
+                                .functions(
+                                                "apartmentOperation", // 根据公寓名称、省份名称、城市名称、区域名称、公寓介绍、查询对应的公寓信息
+                                                "attrOperation", // 根据房间属性名称查询对应的属性值
+                                                "apartmentInfoOperation", // 根据房间属性值查询对应的房间id
+                                                "roomStatusOperation", // 根据房间id查询对应的房间状态
+                                                "facilityOperation", // 根据房间id查询对应的配套信息
+                                                "roomInfoOperation", // 根据房间id查询对应的房间信息
+                                                "roomsByApartmentNameOperation", // 根据公寓名称查询所有房间号
+                                                "roomByRentRangeOperation", // 根据租金范围查询房间
+                                                "roomByPaymentTypeOperation", // 根据付款方式查询房间
+                                                "roomByLeaseTermOperation", // 根据租期查询房间
+                                                "availableRoomsByApartmentIdOperation", // 根据公寓ID查询可租房源
+                                                "leaseTermInfoOperation", // 根据房间ID查询租期信息
+                                                "paymentTypeInfoOperation", // 根据房间ID查询付款方式
+                                                "apartmentDetailInfoOperation" // 根据公寓ID查询公寓详细信息
+                                )
+                                .stream()
+                                .content();
 
         System.out.println("ai助手：" + content);
 
